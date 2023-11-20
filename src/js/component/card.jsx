d7c76eb82562fd80af91f6045986e9b5769d007f
richardgarciaordaz <64115217+richardgarciaordaz@users.noreply.github.com>
2023-11-20 17:02:06 +0000

    Landing

---
+++ src/js/component/card.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Card =  (props) => {
+return ( 
+    <>
+    <div className="card" style={{width: "18rem"}}>
+  <img src={props.imagen} className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">{props.titulo}</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+    </>
+
+)
+}
+
+export default Card;
---
+++ src/js/component/footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer =  () => {
+return ( 
+    <>
+    <div className="card-footer bg-dark" style={{color:"white"}}>
+        Richard
+
+    </div>
+    </>
+
+
+
+)
+}
+
+export default Footer;
